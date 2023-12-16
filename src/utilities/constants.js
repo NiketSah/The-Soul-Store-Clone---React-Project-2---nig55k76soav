caebbffccaab9255ffb5218afb322717ddd92349
NiketSah <sahniket15@gmail.com>
2023-12-16 17:50:29 +0530

    few changes on jwt

---
+++ src/utilities/constants.js
@@ -0,0 +1,7 @@
+export const Product_API =
+  "https://academics.newtonschool.co/api/v1/ecommerce/clothes/products";
+export const projectId = "nig55k76soav";
+export const ProductInfo_API =
+  "https://academics.newtonschool.co/api/v1/ecommerce/product/";
+export const EachOrder_API =
+  "https://academics.newtonschool.co/api/v1/ecommerce/order/";
